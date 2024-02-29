1841187780d532df99c384201e17631f0487762e
raka334 <readytogave@outlook.com>
2024-03-01 02:13:38 +0530

    Navbar minor change

--- apps/web-punjab/components/navbar.js
+++ apps/web-punjab/components/navbar.js
@@ -4,9 +4,9 @@ document.querySelector('#navbar').innerHTML = `
 <div>
 <div class="navbar">
 <img class="left" src="./wow-logo.png" />
-<a href="#home">Home</a>
-<a href="#news">News</a>
-<a href="#contact">Contact</a>
 <a href="#about">About</a>
+<a href="#contact">Contact</a>
+<a href="#news">News</a>
+<a href="#home">Home</a>
 </div>
 `
