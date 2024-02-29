c983c037517196a42412ec2dbf3eb28c4d4a783f
raka334 <readytogave@outlook.com>
2024-03-01 01:57:41 +0530

    initial project setup

---
+++ apps/web-punjab/components/navbar.js
@@ -0,0 +1,12 @@
+import '/components/navbar.css'
+
+document.querySelector('#navbar').innerHTML = `
+<div>
+<div class="navbar">
+<img class="left" src="./wow-logo.png" />
+<a href="#home">Home</a>
+<a href="#news">News</a>
+<a href="#contact">Contact</a>
+<a href="#about">About</a>
+</div>
+`
