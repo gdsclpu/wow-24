e45aa86ff8c283f7ff61eff4a1b3cf7cfa2bbe31
Sawan <itissaavn@gmail.com>
2024-03-18 22:02:15 +0530

    Add: hero section

---
+++ apps/web-punjab/components/hero.js
@@ -0,0 +1,17 @@
+import "../style.css"
+
+document.querySelector('#hero').innerHTML = `
+<section>
+<div class="hero-container container">
+<div class="hero-container-overlay container">
+<div class="bg one"></div>
+<div class="bg two"></div>
+<div class="bg three"></div>
+<div class="bg four"></div>
+<div class="bg five"></div>
+<div class="bg six"></div>
+<div class="bg seven"></div>
+</div>
+</div>
+</section>
+`
