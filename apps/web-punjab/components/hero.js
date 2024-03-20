90606a45e547f900e30f416b7ecfe4ca613729a4
Aryan Kumar <aryan.ak434@gmail.com>
2024-03-20 15:44:42 +0530

    add: temp hero section

---
+++ apps/web-punjab/components/hero.js
@@ -0,0 +1,15 @@
+import "../style.css";
+
+document.querySelector("#hero").innerHTML = `
+<div class="flex-row-align-start-container container">
+  <div class="section">
+    <h1 class="third-color headline">Dil Kehnda WOW!</h1>
+    <p class="multi-paragraph">
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+    </p>
+    <button class="ts_bookNow_button">Get Tickets Now</button>
+  </div>
+  <div class="section">
+  <img src="/images/makeConnectionsImage.png" alt="Make Connections">
+  </div>
+</div>`;
